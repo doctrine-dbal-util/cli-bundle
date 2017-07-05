6bc7a99dc5c11b30554ac1de468c1e0ce78266b6
Nardberjean <Nardberjean@users.noreply.github.com>
2017-07-05 12:38:57 -0400

    Create DbalListDatabasesCommand.php

---
+++ Command/DbalListDatabasesCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DbalUtil\CliBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DbalListDatabasesCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('dbal:list-databases')
+            ->setDescription('List Default Connection Databases.')
+            // ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            // ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // $argument = $input->getArgument('argument');
+
+        // if ($input->getOption('option')) {
+            // ...
+        // }
+
+        $output->writeln($this
+            // ->getDBAL()
+            ->getContainer()->get('doctrine.dbal.default_connection')
+            ->getSchemaManager()
+            ->listDatabases()); // Tested OK with Postgres.
+            // TODO: see how to output an array.
+    }
+
+}
