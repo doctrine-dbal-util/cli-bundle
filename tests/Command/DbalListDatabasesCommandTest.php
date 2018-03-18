b562e79a5dffe32f90d56bf93b952d9d2d6be3d7
Nardberjean <Nardberjean@users.noreply.github.com>
2018-03-18 10:36:55 -0400

    Create DbalListDatabasesCommandTest.php

---
+++ tests/Command/DbalListDatabasesCommandTest.php
@@ -0,0 +1,35 @@
+namespace Tests\Command;
+
+use DoctrineDbalUtil\CliBundle\Command\DbalListDatabasesCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class DbalListDatabasesCommandTest extends KernelTestCase
+{
+    public function testExecute()
+    {
+        self::bootKernel();
+        $application = new Application(self::$kernel);
+
+        $application->add(new DbalListDatabasesCommand());
+
+        $command = $application->find('dbal:list-databases');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+
+            // pass arguments to the helper
+            //...'username' => 'Wouter',
+
+            // prefix the key with two dashes when passing options,
+            // e.g: '--some-option' => 'option_value',
+        ));
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        // $this->assertContains('Username: Wouter', $output);
+
+        // ...
+    }
+}
